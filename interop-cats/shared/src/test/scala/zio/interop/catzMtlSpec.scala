0f97463102e752587d6486b1f59a18d8e156c861
Georgi Krastev <joro.kr.21@gmail.com>
2019-08-29 13:08:58 +0200

    Enable interop for scala.js (#28)
    * Enable interop for scala.js
    
      * Test more thoroughly
      * Delete `Alternative` instance (unlawful)
      * Simplify some method implementations
    
    * Disable failing cats-effect laws for the time being
    
      * `bracketReleaseIsCalledOnCompletedOrError`
      * `runCancelableIsSynchronous`
    
    * Add a test for summoning Monad[UIO]
    
    * Define maximum memory limit in .jvmopts
    
    * Increase no_output_timeout for scala.js on 2.13
    
    * Override runCancelableIsSynchronous
    
    * override bracketReleaseIsCalledOnCompletedOrError (replace F.pure(b) with fa.attempt.as(b))
    
    * Silence errors from runCancelable/runAsync same as cats-IO impl does. (unsafeRunAsyncAndForget logs errors via EC)
    
    * Further bump JVM limits in .jvmopts
    
    * Further bump JVM limits
    
    * Fix cause of flaky equality – zipPar is not deterministic; replace genParallel by ZManaged.fork-based version; split out shared test implicits; re-add repeat tests for ApplicativeLocal & ConcurrentEffect
    
    * inherit AnyFunSuite directly in test base
    
    * fix build
    
    * remove test loops
    
    * decrease heap to 4G
    
    * duplicate .jvmopts in .sbtopts
    
    * remove redundant .jvmopts

---
+++ interop-cats/shared/src/test/scala/zio/interop/catzMtlSpec.scala
@@ -0,0 +1,38 @@
+package zio.interop
+
+import cats.implicits._
+import cats.mtl.laws.discipline._
+import cats.mtl.{ ApplicativeAsk, ApplicativeHandle, ApplicativeLocal, FunctorRaise }
+import zio._
+import zio.interop.catz._
+import zio.interop.catz.mtl._
+
+class catzMtlSpec extends catzSpecBase {
+  type Ctx   = String
+  type Error = String
+
+  checkAllAsync(
+    "ApplicativeAsk[ZIO[Ctx, Error, ?]]",
+    implicit tc => ApplicativeAskTests[ZIO[Ctx, Error, ?], Ctx].applicativeAsk[Ctx]
+  )
+
+  checkAllAsync(
+    "ApplicativeLocal[ZIO[Ctx, Error, ?]]",
+    implicit tc => ApplicativeLocalTests[ZIO[Ctx, Error, ?], Ctx].applicativeLocal[Ctx, Int]
+  )
+
+  checkAllAsync(
+    "FunctorRaise[ZIO[Ctx, Error, ?]]",
+    implicit tc => FunctorRaiseTests[ZIO[Ctx, Error, ?], Error].functorRaise[Int]
+  )
+
+  checkAllAsync(
+    "ApplicativeHandle[ZIO[Ctx, Error, ?]]",
+    implicit tc => ApplicativeHandleTests[ZIO[Ctx, Error, ?], Error].applicativeHandle[Int]
+  )
+
+  def askSummoner[R, E]    = ApplicativeAsk[ZIO[R, E, ?], R]
+  def localSummoner[R, E]  = ApplicativeLocal[ZIO[R, E, ?], R]
+  def raiseSummoner[R, E]  = FunctorRaise[ZIO[R, E, ?], E]
+  def handleSummoner[R, E] = ApplicativeHandle[ZIO[R, E, ?], E]
+}
