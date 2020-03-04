975d9ed3d3b167029e3e65e5c5c4502c9e82d8c1
Pierangelo Cecchetto <pierangeloc@gmail.com>
2020-03-04 11:53:39 +0100

    Use * notation for kind projector

--- interop-cats/shared/src/test/scala/zio/interop/catzSpec.scala
+++ interop-cats/shared/src/test/scala/zio/interop/catzSpec.scala
@@ -26,12 +26,12 @@ class catzSpec extends catzSpecZIOBase {
   )
   checkAllAsync("Effect[Task]", implicit tc => EffectTestsOverrides[Task].effect[Int, Int, Int])
   checkAllAsync("Concurrent[Task]", implicit tc => ConcurrentTestsOverrides[Task].concurrent[Int, Int, Int])
-  checkAllAsync("MonadError[IO[Int, ?]]", implicit tc => MonadErrorTests[IO[Int, ?], Int].monadError[Int, Int, Int])
-  checkAllAsync("MonoidK[IO[Int, ?]]", implicit tc => MonoidKTests[IO[Int, ?]].monoidK[Int])
-  checkAllAsync("SemigroupK[IO[Option[Unit], ?]]", implicit tc => SemigroupKTests[IO[Option[Unit], ?]].semigroupK[Int])
+  checkAllAsync("MonadError[IO[Int, *]]", implicit tc => MonadErrorTests[IO[Int, *], Int].monadError[Int, Int, Int])
+  checkAllAsync("MonoidK[IO[Int, *]]", implicit tc => MonoidKTests[IO[Int, *]].monoidK[Int])
+  checkAllAsync("SemigroupK[IO[Option[Unit], *]]", implicit tc => SemigroupKTests[IO[Option[Unit], *]].semigroupK[Int])
   checkAllAsync("SemigroupK[Task]", implicit tc => SemigroupKTests[Task].semigroupK[Int])
   checkAllAsync("Bifunctor[IO]", implicit tc => BifunctorTests[IO].bifunctor[Int, Int, Int, Int, Int, Int])
-  checkAllAsync("Parallel[Task]", implicit tc => ParallelTests[Task, ParIO[Any, Throwable, ?]].parallel[Int, Int])
+  checkAllAsync("Parallel[Task]", implicit tc => ParallelTests[Task, ParIO[Any, Throwable, *]].parallel[Int, Int])
   checkAllAsync("Monad[UIO]", { implicit tc =>
     implicit def ioArbitrary[A: Arbitrary: Cogen]: Arbitrary[UIO[A]] = Arbitrary(genUIO[A])
     MonadTests[UIO].apply[Int, Int, Int]
@@ -42,41 +42,41 @@ class catzSpec extends catzSpecZIOBase {
   )
 
   // ZManaged Tests
-  checkAllAsync("Monad[ZManaged]", implicit tc => MonadTests[ZManaged[Any, Throwable, ?]].apply[Int, Int, Int])
-  checkAllAsync("Monad[ZManaged]", implicit tc => ExtraMonadTests[ZManaged[Any, Throwable, ?]].monadExtras[Int])
-  checkAllAsync("SemigroupK[ZManaged]", implicit tc => SemigroupKTests[ZManaged[Any, Throwable, ?]].semigroupK[Int])
+  checkAllAsync("Monad[ZManaged]", implicit tc => MonadTests[ZManaged[Any, Throwable, *]].apply[Int, Int, Int])
+  checkAllAsync("Monad[ZManaged]", implicit tc => ExtraMonadTests[ZManaged[Any, Throwable, *]].monadExtras[Int])
+  checkAllAsync("SemigroupK[ZManaged]", implicit tc => SemigroupKTests[ZManaged[Any, Throwable, *]].semigroupK[Int])
   checkAllAsync(
     "MonadError[ZManaged]",
-    implicit tc => MonadErrorTests[ZManaged[Any, Int, ?], Int].monadError[Int, Int, Int]
+    implicit tc => MonadErrorTests[ZManaged[Any, Int, *], Int].monadError[Int, Int, Int]
   )
-  checkAllAsync("Sync[ZManaged]", implicit tc => SyncTests[ZManaged[Any, Throwable, ?]].sync[Int, Int, Int])
+  checkAllAsync("Sync[ZManaged]", implicit tc => SyncTests[ZManaged[Any, Throwable, *]].sync[Int, Int, Int])
 
   object summoningInstancesTest {
     import cats._
     import cats.effect._
 
-    Concurrent[RIO[String, ?]]
-    Async[RIO[String, ?]]
-    LiftIO[RIO[String, ?]]
-    Sync[RIO[String, ?]]
-    MonadError[RIO[String, ?], Throwable]
-    Monad[RIO[String, ?]]
-    Applicative[RIO[String, ?]]
-    Functor[RIO[String, ?]]
-    Parallel[RIO[String, ?], ParIO[String, Throwable, ?]]
-    SemigroupK[RIO[String, ?]]
-    implicitly[Parallel[RIO[String, ?]]]
+    Concurrent[RIO[String, *]]
+    Async[RIO[String, *]]
+    LiftIO[RIO[String, *]]
+    Sync[RIO[String, *]]
+    MonadError[RIO[String, *], Throwable]
+    Monad[RIO[String, *]]
+    Applicative[RIO[String, *]]
+    Functor[RIO[String, *]]
+    Parallel[RIO[String, *], ParIO[String, Throwable, *]]
+    SemigroupK[RIO[String, *]]
+    implicitly[Parallel[RIO[String, *]]]
     Apply[UIO]
-    LiftIO[ZManaged[String, Throwable, ?]]
-    MonadError[ZManaged[String, Throwable, ?], Throwable]
-    Monad[ZManaged[String, Throwable, ?]]
-    Applicative[ZManaged[String, Throwable, ?]]
-    Functor[ZManaged[String, Throwable, ?]]
-    SemigroupK[ZManaged[String, Throwable, ?]]
-    Sync[ZManaged[String, Throwable, ?]]
+    LiftIO[ZManaged[String, Throwable, *]]
+    MonadError[ZManaged[String, Throwable, *], Throwable]
+    Monad[ZManaged[String, Throwable, *]]
+    Applicative[ZManaged[String, Throwable, *]]
+    Functor[ZManaged[String, Throwable, *]]
+    SemigroupK[ZManaged[String, Throwable, *]]
+    Sync[ZManaged[String, Throwable, *]]
 
-    def concurrentEffect[R: Runtime] = ConcurrentEffect[RIO[R, ?]]
-    def effect[R: Runtime]           = Effect[RIO[R, ?]]
+    def concurrentEffect[R: Runtime] = ConcurrentEffect[RIO[R, *]]
+    def effect[R: Runtime]           = Effect[RIO[R, *]]
   }
 
   object summoningRuntimeInstancesTest {
@@ -84,7 +84,7 @@ class catzSpec extends catzSpecZIOBase {
     import zio.interop.catz.implicits._
 
     ContextShift[Task]
-    ContextShift[RIO[String, ?]]
+    ContextShift[RIO[String, *]]
     cats.effect.Clock[Task]
     Timer[Task]
 
