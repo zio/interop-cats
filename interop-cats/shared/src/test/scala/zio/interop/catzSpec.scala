42787e8d4defc2f956d451db33723c15e8e86052
Kai <kai@7mind.io>
2020-05-16 18:48:33 +0100

    Test that new `deferInstance` doesn't conflict with cats-effect instances

--- interop-cats/shared/src/test/scala/zio/interop/catzSpec.scala
+++ interop-cats/shared/src/test/scala/zio/interop/catzSpec.scala
@@ -103,6 +103,16 @@ class catzSpec extends catzSpecZIOBase {
     Monad[UIO]
   }
 
+  test("catsCoreSummoningTest") {
+    import catz._
+    import catz.core._
+    // check that core deferInstance does not conflict with taskConcurrentInstance
+    val _     = deferInstance // unused import
+    val defer = cats.Defer[Task]
+    val sync  = cats.effect.Sync[Task]
+    assert(defer eq sync)
+  }
+
   object concurrentEffectSyntaxTest {
     import cats.effect.syntax.all._
 
