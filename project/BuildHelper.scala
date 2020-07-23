5fb59a8ada4da97b2c32b48e4e91373516155843
Scala Steward <me@scala-steward.org>
2020-07-23 17:04:11 +0200

    Update silencer-lib, silencer-plugin to 1.7.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ import BuildInfoKeys._
 
 object BuildHelper {
   val testDeps        = Seq("org.scalacheck"  %% "scalacheck"  % "1.14.3" % Test)
-  val compileOnlyDeps = Seq("com.github.ghik" % "silencer-lib" % "1.7.0"  % Provided cross CrossVersion.full)
+  val compileOnlyDeps = Seq("com.github.ghik" % "silencer-lib" % "1.7.1"  % Provided cross CrossVersion.full)
 
   private val stdOptions = Seq(
     "-deprecation",
@@ -76,7 +76,7 @@ object BuildHelper {
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
       compilerPlugin("org.typelevel"   % "kind-projector"  % "0.11.0") cross CrossVersion.full,
-      compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.0") cross CrossVersion.full
+      compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.1") cross CrossVersion.full
     ),
     parallelExecution in Test := true,
     incOptions ~= (_.withLogRecompileOnMacro(false)),
