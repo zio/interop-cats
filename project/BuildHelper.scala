6a404373dec63bea50853551cbda543c537cbc67
Sviat Chumakov <svchumakov@gmail.com>
2021-04-27 19:53:54 +0300

    Update Scala 3 to 3.0.0-RC3

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,7 +12,7 @@ object BuildHelper {
 
   val Scala212 = "2.12.13"
   val Scala213 = "2.13.5"
-  val Scala3   = "3.0.0-RC2"
+  val Scala3   = "3.0.0-RC3"
 
   private val stdOptions = Seq(
     "-deprecation",
