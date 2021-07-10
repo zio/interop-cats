26eaff8365361d88874e3fdffc81515dce6793d5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-10 04:49:05 +0200

    Update scala3-library to 3.0.1 (#379)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -11,7 +11,7 @@ object BuildHelper {
 
   val Scala212 = "2.12.13"
   val Scala213 = "2.13.6"
-  val Scala3   = "3.0.0"
+  val Scala3   = "3.0.1"
 
   private val stdOptions = Seq(
     "-deprecation",
