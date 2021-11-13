b76bd131eccaa171fc65491517c576c10a53c3cc
calvinlfer <calvin.l.fer@gmail.com>
2021-11-13 14:32:14 -0500

    Update FS2 version to the latest built with Scala 3.0.1 and merge implementations of queue sizes

--- zio-interop-cats/shared/src/main/scala/zio/stream/interop/FS2StreamSyntax.scala
+++ zio-interop-cats/shared/src/main/scala/zio/stream/interop/FS2StreamSyntax.scala
@@ -1,7 +1,7 @@
 package zio
 package stream.interop
 
-import fs2.Stream
+import fs2.{ Pull, Stream }
 import zio.interop.catz.{ concurrentInstance, zManagedSyntax }
 import zio.stream.{ Take, ZStream }
 
@@ -38,10 +38,10 @@ final class FS2RIOStreamSyntax[R, A](private val stream: Stream[RIO[R, _], A]) {
    * @note when possible use only power of 2 queue sizes; this will provide better performance of the queue.
    */
   def toZStream[R1 <: R](queueSize: Int = 16): ZStream[R1, Throwable, A] =
-    if (queueSize > 1) toZStreamChunk(queueSize) else toZStreamSingle
+    if (queueSize > 1) toZStreamChunk(queueSize) else toZStreamChunk(queueSize = 1)
 
   def toZStreamChunk[R1 <: R](queueSize: Int = 16): ZStream[R1, Throwable, A] = {
-    def integrate(stream: fs2.Stream[RIO[R, _], A], zQueue: Queue[Take[Throwable, A]]): fs2.Pull[RIO[R, _], A, Unit] =
+    def integrate(stream: Stream[RIO[R, _], A], zQueue: Queue[Take[Throwable, A]]): Pull[RIO[R, _], A, Unit] =
       stream.pull.uncons.flatMap {
         case None =>
           fs2.Pull.eval(zQueue.offer(Take.end)) >> fs2.Pull.done
@@ -51,31 +51,10 @@ final class FS2RIOStreamSyntax[R, A](private val stream: Stream[RIO[R, _], A]) {
           fs2.Pull.eval(offer) >> integrate(stream, zQueue)
       }
 
-    convert(queueSize, integrate)
-  }
-
-  private def toZStreamSingle[R1 <: R]: ZStream[R1, Throwable, A] = {
-    def integrate(stream: fs2.Stream[RIO[R, _], A], zQueue: Queue[Take[Throwable, A]]): fs2.Pull[RIO[R, _], A, Unit] =
-      stream.pull.uncons.flatMap {
-        case None =>
-          fs2.Pull.eval(zQueue.offer(Take.end)) >> fs2.Pull.done
-
-        case Some((elements, stream)) =>
-          val offer = zQueue.offerAll(toZioChunk(elements).map(Take.single))
-          fs2.Pull.eval(offer) >> integrate(stream, zQueue)
-      }
-
-    convert(1, integrate)
-  }
-
-  private def convert(
-    queueSize: Int,
-    integrate: (fs2.Stream[RIO[R, _], A], Queue[Take[Throwable, A]]) => fs2.Pull[RIO[R, _], A, Unit]
-  ) =
     ZStream.fromEffect(Queue.bounded[Take[Throwable, A]](queueSize)).flatMap { q =>
       val toQueue = ZStream.fromEffect {
         integrate(stream, q).stream
-          .handleErrorWith(e => fs2.Stream.eval(q.offer(Take.fail(e))))
+          .handleErrorWith(e => Stream.eval(q.offer(Take.fail(e))))
           .compile
           .drain
       }
@@ -83,22 +62,17 @@ final class FS2RIOStreamSyntax[R, A](private val stream: Stream[RIO[R, _], A]) {
       val fromQueue = ZStream.fromQueue(q).flattenTake
       fromQueue.drainFork(toQueue)
     }
+  }
 
   private def toZioChunk(in: fs2.Chunk[A]): Chunk[A] =
     in match {
       case fs2.Chunk.ArraySlice(values, _, _) =>
         Chunk.fromArray(values)
 
-      case buffer: fs2.Chunk.Buffer[_, _, _] =>
-        Chunk.fromIterator(buffer.iterator)
-
-      case queue: fs2.Chunk.Queue[a] =>
-        Chunk.fromIterator(queue.iterator)
-
       case singleton: fs2.Chunk.Singleton[a] =>
         Chunk.single(singleton.value)
 
-      case unknown =>
-        Chunk.fromIterable(unknown.toList)
+      case other =>
+        Chunk.fromIterator(other.iterator)
     }
 }
