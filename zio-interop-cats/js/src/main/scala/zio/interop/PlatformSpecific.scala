3d8405266edeb067d21cd449a4c2c8eb991b2ac7
Kai <450507+neko-kai@users.noreply.github.com>
2022-08-16 19:12:48 +0100

    fix build on 2.12 Scala.js

--- zio-interop-cats/js/src/main/scala/zio/interop/PlatformSpecific.scala
+++ zio-interop-cats/js/src/main/scala/zio/interop/PlatformSpecific.scala
@@ -2,12 +2,12 @@ package zio.interop
 
 import zio.RIO
 
-import scala.annotation.unused
-
 private[interop] object PlatformSpecific {
   type CBlocking        = Any
   type CBlockingService = AnyRef
 
-  @inline private[interop] def provideBlocking[A](@unused environment: CBlocking)(fa: RIO[CBlocking, A]): RIO[Any, A] =
+  @inline private[interop] def provideBlocking[A](environment: CBlocking)(fa: RIO[CBlocking, A]): RIO[Any, A] = {
+    val _ = environment
     fa
+  }
 }
