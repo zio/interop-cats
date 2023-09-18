cda6dcb8b9cb47d8a9dff0e485d988a8655ceb94
Aleksei Shamenev <ashamenev@onairent.live>
2023-09-18 20:27:45 +0300

    remove unused defs

--- zio-interop-cats-tests/shared/src/test/scala/zio/interop/CatsMtlSpec.scala
+++ zio-interop-cats-tests/shared/src/test/scala/zio/interop/CatsMtlSpec.scala
@@ -2,7 +2,6 @@ package zio.interop
 
 import cats.implicits.*
 import cats.mtl.laws.discipline.*
-import cats.mtl.{ Handle, Raise }
 import zio.*
 import zio.interop.catz.*
 import zio.interop.catz.mtl.*
@@ -44,7 +43,4 @@ class CatsMtlSpec extends ZioSpecBase {
     "Handle[ZIO[Ctx, Error, _]]",
     implicit tc => HandleTests[ZIO[Ctx, Error, _], Error].handle[Int]
   )
-
-  def raiseSummoner[R, E]  = Raise[ZIO[R, E, _], E]
-  def handleSummoner[R, E] = Handle[ZIO[R, E, _], E]
 }
