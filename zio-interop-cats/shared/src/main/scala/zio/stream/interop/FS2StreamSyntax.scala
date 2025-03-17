zio/interop-cats

commit e09811ee464f3bb1d2941585d13b7a506a779d48

Fix #702 propagate defects correctly in conversion from fs2 to ZStream (#705)
* Fix #702 propagate defects correctly in conversion from fs2 to ZStream

Note: .resource.drain.toScopedZIO caused a hang, not observed with simple .drain. That shouldn't have happened.

* 2.12 compat

* fix build

zio-interop-cats/shared/src/main/scala/zio/stream/interop/FS2StreamSyntax.scala
@@ -2,8 +2,9 @@ package zio
 package stream.interop
 
 import cats.effect.Resource
+import cats.effect.kernel.Resource.ExitCase
 import fs2.Stream
-import zio.interop.catz.{ concurrentInstance, scopedSyntax, zioResourceSyntax }
+import zio.interop.catz.{ concurrentInstance, scopedSyntax }
 import zio.stream.{ Take, ZStream }
 
 import scala.language.implicitConversions
@@ -41,45 +42,35 @@ final class FS2RIOStreamSyntax[R, A](private val stream: Stream[RIO[R, _], A]) {
    *
    * @note when possible use only power of 2 queue sizes; this will provide better performance of the queue.
    */
-  def toZStream[R1 <: R](queueSize: Int = 16)(implicit trace: Trace): ZStream[R1, Throwable, A] =
-    if (queueSize > 1) toZStreamChunk(queueSize) else toZStreamSingle
+  def toZStream[R1 <: R](queueSize: Int = 16)(implicit trace: Trace): ZStream[R1, Throwable, A] = {
+    val useChunkedImpl = queueSize > 1
+    val realQueueSize  = if (useChunkedImpl) queueSize else 1
 
-  private def toZStreamSingle[R1 <: R](implicit trace: Trace): ZStream[R1, Throwable, A] =
+    def streamToQueue(queue: Enqueue[Take[Throwable, A]]): Stream[RIO[R, _], Any] =
+      if (useChunkedImpl) {
+        stream
+          .chunkLimit(queueSize)
+          .evalTap(a => queue.offer(Take.chunk(zio.Chunk.fromIterator(a.iterator))))
+      } else {
+        stream.evalTap(a => queue.offer(Take.single(a)))
+      }
+
     ZStream
       .scoped[R1] {
         for {
-          queue <- ZIO.acquireRelease(Queue.bounded[Take[Throwable, A]](1))(_.shutdown)
-          _     <-
-            (stream.evalTap(a => queue.offer(Take.single(a))) ++ fs2.Stream
-              .eval(queue.offer(Take.end)))
-              .handleErrorWith(e => fs2.Stream.eval(queue.offer(Take.fail(e))).drain)
-              .compile[RIO[R, _], RIO[R, _], Any]
-              .resource
-              .drain
-              .toScopedZIO
-              .forkScoped
+          queue <- ZIO.acquireRelease(Queue.bounded[Take[Throwable, A]](realQueueSize))(_.shutdown)
+          _     <- streamToQueue(queue)
+                     .onFinalizeCase[RIO[R, _]] {
+                       case ExitCase.Succeeded  => queue.offer(Take.end).unit
+                       case ExitCase.Errored(e) => queue.offer(Take.fail(e)).unit
+                       case ExitCase.Canceled   => queue.offer(Take.done(Exit.interrupt(FiberId.None))).unit
+                     }
+                     .compile[RIO[R, _], RIO[R, _], Any]
+                     .drain
+                     .forkScoped
         } yield ZStream.fromQueue(queue).flattenTake
       }
       .flatten
+  }
 
-  private def toZStreamChunk[R1 <: R](queueSize: Int)(implicit trace: Trace): ZStream[R1, Throwable, A] =
-    ZStream
-      .scoped[R1] {
-        for {
-          queue <- ZIO.acquireRelease(Queue.bounded[Take[Throwable, A]](queueSize))(_.shutdown)
-          _     <- {
-            stream
-              .chunkLimit(queueSize)
-              .evalTap(a => queue.offer(Take.chunk(zio.Chunk.fromIterable(a.toList))))
-              .chunkLimit(1)
-              .unchunks ++ fs2.Stream.eval(queue.offer(Take.end))
-          }.handleErrorWith(e => fs2.Stream.eval(queue.offer(Take.fail(e))).drain)
-            .compile[RIO[R, _], RIO[R, _], Any]
-            .resource
-            .drain
-            .toScopedZIO
-            .forkScoped
-        } yield ZStream.fromQueue(queue).flattenTake
-      }
-      .flatten
 }

zio-interop-cats-tests/jvm/src/test/scala/zio/interop/fs2StreamSpec.scala
@@ -1,12 +1,12 @@
 package zio.interop
 
 import fs2.Stream
-import zio.{ Chunk, Ref, Task }
-import zio.stream.ZStream
-import zio.test.Assertion.{ equalTo, fails }
-import zio.test.*
-import zio.interop.catz.*
 import zio.Random.nextIntBetween
+import zio.interop.catz.*
+import zio.stream.ZStream
+import zio.test.*
+import zio.test.Assertion.*
+import zio.{ Chunk, Ref, Task }
 
 object fs2StreamSpec extends ZIOSpecDefault {
   import zio.stream.interop.fs2z.*
@@ -42,6 +42,11 @@ object fs2StreamSpec extends ZIOSpecDefault {
       test("error propagation") {
         val result = ZStream.fail(exception).toFs2Stream.compile.drain.exit
         assertZIO(result)(fails(equalTo(exception)))
+      },
+      test("unguarded throw propagation") {
+        val stream: ZStream[Any, Throwable, Int] = ZStream(1, 2, 3) ++ (throw exception)
+        val result                               = stream.toFs2Stream.compile.drain.exit
+        assertZIO(result)(dies(equalTo(exception)))
       }
     ),
     suite("test toZStream conversion")(
@@ -56,6 +61,10 @@ object fs2StreamSpec extends ZIOSpecDefault {
       }),
       test("error propagation") {
         val result = Stream.raiseError[Task](exception).toZStream().runDrain.exit
+        assertZIO(result)(fails(equalTo(exception)))
+      },
+      test("unguarded throw propagation") {
+        val result = (Stream[Task, Int](1, 2, 3) ++ (throw exception)).toZStream().runDrain.exit
         assertZIO(result)(fails(equalTo(exception)))
       },
       test("releases all resources by the time the failover stream has started") {

commit 9118c486f9bcd46613464a15e090d3f2a4fd67bd

Follow up #705, use ZIO.onExit instead of fs2.Stream.onFinalizeCase to more robustly catch all cases
I thought that interruption inheritance could've caused hangs in #705 when .resource.drain.toScopedZIO was used. That doesn't seem to be the case, instead termination happened before onFinalizeCase error handler was installed. Using native ZIO onExit prevents that from happening and also prevents conversion of Cause.Die to Cause.Error.

zio-interop-cats/shared/src/main/scala/zio/stream/interop/FS2StreamSyntax.scala
@@ -2,7 +2,6 @@ package zio
 package stream.interop
 
 import cats.effect.Resource
-import cats.effect.kernel.Resource.ExitCase
 import fs2.Stream
 import zio.interop.catz.{ concurrentInstance, scopedSyntax }
 import zio.stream.{ Take, ZStream }
@@ -60,13 +59,12 @@ final class FS2RIOStreamSyntax[R, A](private val stream: Stream[RIO[R, _], A]) {
         for {
           queue <- ZIO.acquireRelease(Queue.bounded[Take[Throwable, A]](realQueueSize))(_.shutdown)
           _     <- streamToQueue(queue)
-                     .onFinalizeCase[RIO[R, _]] {
-                       case ExitCase.Succeeded  => queue.offer(Take.end).unit
-                       case ExitCase.Errored(e) => queue.offer(Take.fail(e)).unit
-                       case ExitCase.Canceled   => queue.offer(Take.done(Exit.interrupt(FiberId.None))).unit
-                     }
                      .compile[RIO[R, _], RIO[R, _], Any]
                      .drain
+                     .onExit {
+                       case Exit.Success(_)           => queue.offer(Take.end)
+                       case failure @ Exit.Failure(_) => queue.offer(Take.done(failure))
+                     }
                      .forkScoped
         } yield ZStream.fromQueue(queue).flattenTake
       }

zio-interop-cats-tests/jvm/src/test/scala/zio/interop/fs2StreamSpec.scala
@@ -65,7 +65,7 @@ object fs2StreamSpec extends ZIOSpecDefault {
       },
       test("unguarded throw propagation") {
         val result = (Stream[Task, Int](1, 2, 3) ++ (throw exception)).toZStream().runDrain.exit
-        assertZIO(result)(fails(equalTo(exception)))
+        assertZIO(result)(dies(equalTo(exception)))
       },
       test("releases all resources by the time the failover stream has started") {
         for {